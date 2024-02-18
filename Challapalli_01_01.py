f0d58c8bdc215a3e42411ad64e360275b91e61ea
AbhijitChallapalli <saiabhijit.1234@gmail.com>
2024-02-18 17:32:39 -0600

    Second_Commit

--- Challapalli_01_01.py
+++ Challapalli_01_01.py
@@ -1,48 +1,3 @@
-# # Kamangar, Farhad
-# # 1000_123_456
-# # 2024_02_11
-# # Assignment_01_01
-
-# import numpy as np
-
-# def multi_layer_nn(X_train,Y_train,X_test,Y_test,layers,alpha,epochs,h=0.00001,seed=2):
-#     # This function creates and trains a multi-layer neural Network
-#     # X_train: Array of input for training [input_dimensions,nof_train_samples]
-
-#     # Y_train: Array of desired outputs for training samples [output_dimensions,nof_train_samples]
-#     # X_test: Array of input for testing [input_dimensions,nof_test_samples]
-#     # Y_test: Array of desired outputs for test samples [output_dimensions,nof_test_samples]
-#     # layers: array of integers representing number of nodes in each layer
-#     # alpha: learning rate
-#     # epochs: number of epochs for training.
-#     # h: step size
-#     # seed: random number generator seed for initializing the weights.
-#     # return: This function should return a list containing 3 elements:
-#         # The first element of the return list should be a list of weight matrices.
-#         # Each element of the list should be a 2-dimensional numpy array corresponds to the weight matrix of the corresponding layer.
-
-#         # The second element should be a one dimensional numpy array of numbers
-#         # representing the average mse error after each epoch. Each error should
-#         # be calculated by using the X_test array while the network is frozen.
-#         # This means that the weights should not be adjusted while calculating the error.
-
-#         # The third element should be a two-dimensional numpy array [output_dimensions,nof_test_samples]
-#         # representing the actual output of network when X_test is used as input.
-
-#     # Notes:
-#     # DO NOT use any other package other than numpy
-#     # Bias should be included in the weight matrix in the first column.
-#     # Assume that the activation functions for all the layers are sigmoid.
-#     # Use MSE to calculate error.
-#     # Use gradient descent for adjusting the weights.
-#     # use centered difference approximation to calculate partial derivatives.
-#     # (f(x + h)-f(x - h))/2*h
-#     # Reseed the random number generator when initializing weights for each layer.
-#     # i.e., Initialize the weights for each layer by:
-
-
-
-
 import numpy as np
 
 def sigmoid(x):
@@ -76,12 +31,12 @@ def calculate_gradient(X, Y, weights, h):
         gradient = np.zeros_like(w)
         for i in range(w.shape[0]):
             for j in range(w.shape[1]):
-                # Positive perturbation
+                # Positive Step
                 w[i, j] += h
                 positive_output = forward_propagation(X, weights)[-1]
                 positive_error = calculate_error(Y, positive_output)
 
-                # Negative perturbation
+                # Negative Step
                 w[i, j] -= 2 * h
                 negative_output = forward_propagation(X, weights)[-1]
                 negative_error = calculate_error(Y, negative_output)
